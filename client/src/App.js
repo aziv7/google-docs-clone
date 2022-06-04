682b5dffe1f863250c0116b8dc61dd7b0211a568
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-06-04 22:44:18 +0100

    routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Editor from './Editor';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   return (
-    <div className=''>
-      <Editor />
-    </div>
+    <Router className=''>
+      <Routes>
+        <Route path='/' element={<Navigate to={`/doc/${uuidv4()}`} />} />
+        <Route path='/doc/:id' element={<Editor />} />
+      </Routes>
+    </Router>
   );
 }
 
